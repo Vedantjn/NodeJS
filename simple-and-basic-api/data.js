2006f18a9a6a6736c3cecb4aa57da9ed81d15f27
Vedant Jain <vedant.jain.indore@gmail.com>
2023-08-02 17:10:33 +0530

    4

---
+++ simple-and-basic-api/data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name : 'Vedant', email : 'vedant@gmail.com', contact :1111 },
+    {name : 'Sam', email : 'sam@gmail.com', contact : 4444},
+    {name : 'Peter', email : 'peter@gmail.com', contact : 3333},
+    {name : 'Max', email : 'max@gmail.com', contact : 2222}
+];
+module.exports = data;
