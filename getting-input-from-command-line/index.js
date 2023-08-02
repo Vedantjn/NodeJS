9d4c4fbea10aeaeed65f10af6a2bb23762dac4de
Vedant Jain <vedant.jain.indore@gmail.com>
2023-08-02 17:31:39 +0530

    5

---
+++ getting-input-from-command-line/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const input = process.argv;
+
+if(input[2]=='add')
+{
+    fs.writeFileSync(input[3],input[4])
+}
+else if(input[2]=='remove')
+{
+    fs.unlinkSync(input[3])
+}
+else
+{
+    console.log("invalid input ")
+}
+
+// node index.js add data.txt 'this is color'
+// node index.js remove data.txt
