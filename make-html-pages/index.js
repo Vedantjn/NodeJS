cd24608c26b11f7f389cd7b4109d0864134398e1
Vedant Jain <vedant.jain.indore@gmail.com>
2023-08-03 09:51:12 +0530

    16

--- make-html-pages/index.js
+++ make-html-pages/index.js
@@ -4,6 +4,31 @@ const path = require('path');
 const app = express();
 const publicPath=path.join(__dirname,'public')
 
-app.use(express.static(publicPath));
+app.set('view engine','ejs');
+
+app.get('',(_,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+});
+
+app.get('/profile',(_,resp)=>{
+    const user={
+        name:'Peter',
+        email:'peter@test.com',
+        country:'USA'
+    }
+   resp.render('profile',{user})
+});
+
+app.get('/contact',(_,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+});
+
+app.get('/help',(_,resp)=>{
+    resp.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('*',(_,resp)=>{
+    resp.sendFile(`${publicPath}/nopage.html`)
+});
 
 app.listen(5000);
