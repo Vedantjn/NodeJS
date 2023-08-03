d3aae1d4f1260137d9e738f4c9589a60a8816365
Vedant Jain <vedant.jain.indore@gmail.com>
2023-08-03 13:09:24 +0530

    21

---
+++ read-data-from-mongodb/index.js
@@ -0,0 +1,34 @@
+// const {MongoClient} = require('mongodb')
+// const url= 'mongodb://localhost:27017';
+// const databaseName='e-comm'
+// const client= new MongoClient(url);
+
+// async function getData()
+// {
+//     let result = await client.connect();
+//     db= result.db(databaseName);
+//     collection = db.collection('products');
+//     let data = await collection.find({name:'U10'}).toArray();
+//     console.log(data)
+
+
+// }
+
+// getData();
+
+
+const dbConnect= require('./mongodb');
+
+dbConnect().then((resp)=>{
+resp.find({name:'U10'}).toArray().then((data)=>{
+console.log(data)
+})
+})
+
+const main=async ()=>{
+   let data = await dbConnect();
+   data = await data.find({name:'U10'}).toArray();
+   console.log(data)
+}
+
+main()
